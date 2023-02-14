46acab5c3362c538e21e6af2c7c1c25ec8bb1e1d
Shefali <shefali110500@gmail.com>
2023-02-14 17:07:13 +0530

    My third commit

--- src/com/signify/demo/Demo.java
+++ src/com/signify/demo/Demo.java
@@ -4,7 +4,7 @@ public class Demo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("My second java project");
+		System.out.println("My second java project.");
 	}
 
 }
